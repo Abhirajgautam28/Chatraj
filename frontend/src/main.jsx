1e5dcf0118fe877d46ce1943c304ccfe3bd768d5
ABHIRAJ GAUTAM <abhirajgautam28@gmail.com>
2025-08-24 12:07:57 +0530

    Add speed-insights script to collect performance metrics.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import 'animate.css'
-import './index.css'
-import 'remixicon/fonts/remixicon.css'
-import App from './App.jsx'
+
+import { createRoot } from 'react-dom/client';
+import 'animate.css';
+import './index.css';
+import 'remixicon/fonts/remixicon.css';
+import App from './App.jsx';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')).render(
-
-  <App />
-
-)
+  <>
+    <App />
+    <SpeedInsights />
+  </>
+);
