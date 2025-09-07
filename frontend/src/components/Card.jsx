06f5dff7cec138b70a36db57094a06765de33710
ABHIRAJ GAUTAM <abhirajgautam28@gmail.com>
2025-09-07 11:01:14 +0530

    Fix card background for dark mode.

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Card = ({ className = '', children }) => (
-  <div className={`p-6 rounded-lg shadow-md bg-white dark:bg-card-dark-mode-gradient ${className}`}>
+  <div className={`p-6 rounded-lg shadow-md dark:bg-card-dark-mode-gradient ${className}`}>
     {children}
   </div>
 );
