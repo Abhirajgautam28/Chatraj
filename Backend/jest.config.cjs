3731a149eaf00cf70baed9a33649fadcd22c73fe
ABHIRAJ GAUTAM <abhirajgautam28@gmail.com>
2025-12-18 19:34:17 +0530

    fix(backend): init router and remove explicit app.options; ignore Backend/node_modules

--- Backend/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-};
--- Backend/routes/newsletter.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { subscribeNewsletter } from '../controllers/newsletter.controller.js';
-
-const router = express.Router();
-
-// POST /api/newsletter/subscribe
-router.post('/subscribe', subscribeNewsletter);
-
-export default router;
